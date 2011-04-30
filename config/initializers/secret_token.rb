006a5e3467a95d89b62dd1cdc55b3b09734ca482
John Carney <john@carney.id.au>
2011-04-30 16:15:58 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CouponRails::Application.config.secret_token = 'e1b2d5821ebdf41f9e2a773f04da7c1597f087dbb07b60b06a36c1fd81cb0d3732120e10508e17fdf66e05324e2950df98bc0708fde513795812f2f8aa850771'
