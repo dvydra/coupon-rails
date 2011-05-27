4b47d8ac14632bc4ed76427557f4214e7672a8c2
John Carney <john.carney@lonelyplanet.com.au>
2011-05-27 14:46:49 +1000

    Work-in-progress

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 require "bundler/capistrano"
 
 set :default_stage, "devint"
-set :stages, %w{ devint busprev load prod }
+set :stages, %w{ devint busprev loadtest prod }
 
 require "capistrano/ext/multistage"
 
@@ -13,12 +13,14 @@ set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 set :user, "johnc"
+set :deploy_to, "/var/www/html/apps/#{application}"
 set :deploy_via, :remote_cache
+set :use_sudo, true
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
+  task :restart, :roles => :web, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
